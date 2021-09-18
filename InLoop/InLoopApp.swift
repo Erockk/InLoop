583953a42bf67891e3dec3fbfebc5345e36ce5ce
Erik <erikolson82@gmail.com>
2021-09-18 11:30:23 -0600

    Initial Commit

---
+++ InLoop/InLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  InLoopApp.swift
+//  InLoop
+//
+//  Created by Erik Olson on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct InLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
